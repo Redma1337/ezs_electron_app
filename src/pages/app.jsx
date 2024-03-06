bd4de5833bcf1671db33e8876ead80aa0155b40d
Redma1337 <battlefield.noob66@gmail.com>
2024-03-06 13:59:25 +0100

    refactor: fuck webpack, switch to vite & react support

---
+++ src/pages/app.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App = () => {
+    return (
+        <div className="p-2 px-4 shadow">
+            <h2>Hello from React!</h2>
+        </div>
+    )
+}
+
+export default App;
