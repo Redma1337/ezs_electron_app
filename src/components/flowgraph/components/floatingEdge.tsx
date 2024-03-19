e4dac1c08384000479b94da64f9846223b8c06f5
Redma1337 <battlefield.noob66@gmail.com>
2024-03-19 23:42:28 +0100

    fix: node determines direction and doesnt do unnecessary renders

--- src/components/flowgraph/components/floatingEdge.tsx
+++ src/components/flowgraph/components/floatingEdge.tsx
@@ -1,15 +1,8 @@
 import React, {memo, useCallback} from 'react';
-import {BaseEdge, EdgeProps, getStraightPath, Position, ReactFlowState, useStore} from 'reactflow';
-import {getParallelEdgeParams} from "../../../utils/mathUtils";
+import {BaseEdge, EdgeProps, ReactFlowState, useStore} from 'reactflow';
+import {getBetterFloatingStraightPath} from "../../../utils/mathUtils";
 
-export type GetSpecialPathParams = {
-    sourceX: number;
-    sourceY: number;
-    targetX: number;
-    targetY: number;
-};
-
-const FloatingEdge = memo(({ id, source, target, markerEnd, style } : EdgeProps) => {
+const FloatingEdge = memo(({id, source, target, markerEnd, style }: EdgeProps) => {
     const sourceNode = useStore(useCallback((store) => store.nodeInternals.get(source), [source]));
     const targetNode = useStore(useCallback((store) => store.nodeInternals.get(target), [target]));
 
@@ -18,48 +11,32 @@ const FloatingEdge = memo(({ id, source, target, markerEnd, style } : EdgeProps)
     }
 
     const isBiDirectionEdge = useStore((s: ReactFlowState) => {
-        const edgeExists = s.edges.some(
+        return s.edges.some(
             (e) =>
                 (e.source === target && e.target === source) || (e.target === source && e.source === target)
         );
-
-        return edgeExists;
     });
 
-    const { intersects, sourcePos } = getParallelEdgeParams(sourceNode, targetNode, 30);
-    const { original, parallel } = intersects;
-    let path = "";
-    if (isBiDirectionEdge) {
-        if (sourcePos == Position.Left) {
-            [path] = getStraightPath({
-                sourceX: parallel.sourceIntersect.x,
-                sourceY: parallel.sourceIntersect.y,
-                targetX: parallel.targetIntersect.x,
-                targetY: parallel.targetIntersect.y,
-            });
-            console.log("parallel left");
-            console.log(path);
-        } else {
-
-        }
-    } else {
-        [path] = getStraightPath({
-            sourceX: original.sourceIntersect.x,
-            sourceY: original.sourceIntersect.y,
-            targetX: original.targetIntersect.x,
-            targetY: original.targetIntersect.y,
-        });
-    }
-
+    const { path, parallelPath } = getBetterFloatingStraightPath(targetNode, sourceNode, true, 20);
 
     return (
         <>
-            <BaseEdge
-                id={id}
-                path={path}
-                markerEnd={markerEnd}
-                style={style}
-            />
+            {
+                isBiDirectionEdge && sourceNode.id < targetNode.id ?
+                    <BaseEdge
+                        id={id}
+                        path={path}
+                        markerEnd={markerEnd}
+                        style={style}
+                    />
+                :
+                    <BaseEdge
+                        id={id}
+                        path={parallelPath}
+                        markerEnd={markerEnd}
+                        style={style}
+                    />
+            }
         </>
     );
 })
