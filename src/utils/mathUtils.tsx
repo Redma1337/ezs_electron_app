71268961331063a49c19dd367b7236bdff1a953f
Redma1337 <battlefield.noob66@gmail.com>
2024-03-19 23:43:27 +0100

    fix: rewrite the math utils of react-flow to support intersection of lines not just from the center of node

--- src/utils/mathUtils.tsx
+++ src/utils/mathUtils.tsx
@@ -1,147 +1,127 @@
-import {Node, Position, XYPosition} from 'reactflow';
+import {Node, XYPosition} from 'reactflow';
 
-function createParallelLine(x1: number, y1: number, x2: number, y2: number, offsetDistance: number): { newX1: number; newY1: number; newX2: number; newY2: number } {
-    // Ensure that the first point is always the one with the lower x-value
-    // or if equal, the lower y-value to ensure consistency
-    if (x1 > x2 || (x1 === x2 && y1 > y2)) {
-        [x1, x2] = [x2, x1];
-        [y1, y2] = [y2, y1];
+function createParallelLine(p1: XYPosition, p2: XYPosition, offsetDistance: number): { parallelP1: XYPosition; parallelP2: XYPosition } {
+    if (p1.x > p2.x || (p1.x === p2.x && p1.y > p2.y)) {
+        [p1, p2] = [p2, p1];
     }
 
-    // Calculate the direction vector from (x1, y1) to (x2, y2)
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-
-    // Calculate a unit vector that's perpendicular to the direction vector
-    const magnitude = Math.sqrt(dx ** 2 + dy ** 2);
-    const perpVectorX = -dy / magnitude;
-    const perpVectorY = dx / magnitude;
-
-    [x1, x2] = [x2, x1];
-    [y1, y2] = [y2, y1];
-
-    // Calculate the new coordinates by moving along the perpendicular vector
-    const newX1 = x1 + perpVectorX * offsetDistance;
-    const newY1 = y1 + perpVectorY * offsetDistance;
-    const newX2 = x2 + perpVectorX * offsetDistance;
-    const newY2 = y2 + perpVectorY * offsetDistance;
-
-    return { newX1, newY1, newX2, newY2 };
-}
-
-
-// this helper function returns the intersection point
-// of the line between the center of the intersectionNode and the target node
-function getParallelNodeIntersection(intersectionNode: Node, targetNode: Node, distance: number) {
-    // https://math.stackexchange.com/questions/1724792/an-algorithm-for-finding-the-intersection-point-between-a-center-of-vision-and-a
-    const {
-        width: intersectionNodeWidth,
-        height: intersectionNodeHeight,
-        positionAbsolute: intersectionNodePosition,
-    } = intersectionNode;
-
-    const targetPosition = targetNode.positionAbsolute;
-
-    const w = intersectionNodeWidth / 2;
-    const h = intersectionNodeHeight / 2;
-
-    let x2 = intersectionNodePosition.x + w;
-    let y2 = intersectionNodePosition.y + h;
-    let x1 = targetPosition.x + targetNode.width / 2;
-    let y1 = targetPosition.y + targetNode.height / 2;
-
-    const newPos = createParallelLine(
-        x1, y1, x2, y2,
-        distance
-    );
-
-    x1 = newPos.newX1
-    x2 = newPos.newX2
-    y1 = newPos.newY1
-    y2 = newPos.newY2
-
-    const xx1 = (x1 - x2) / (2 * w) - (y1 - y2) / (2 * h);
-    const yy1 = (x1 - x2) / (2 * w) + (y1 - y2) / (2 * h);
-    const a = 1 / (Math.abs(xx1) + Math.abs(yy1));
-    const xx3 = a * xx1;
-    const yy3 = a * yy1;
-    const x = w * (xx3 + yy3) + x2;
-    const y = h * (-xx3 + yy3) + y2;
-
-    return { x, y };
-}
-
-function getNodeIntersection(intersectionNode: Node, targetNode: Node) {
-    // https://math.stackexchange.com/questions/1724792/an-algorithm-for-finding-the-intersection-point-between-a-center-of-vision-and-a
-    const {
-        width: intersectionNodeWidth,
-        height: intersectionNodeHeight,
-        positionAbsolute: intersectionNodePosition,
-    } = intersectionNode;
-    const targetPosition = targetNode.positionAbsolute;
-
-    const w = intersectionNodeWidth / 2;
-    const h = intersectionNodeHeight / 2;
-
-    const x2 = intersectionNodePosition.x + w;
-    const y2 = intersectionNodePosition.y + h;
-    const x1 = targetPosition.x + targetNode.width / 2;
-    const y1 = targetPosition.y + targetNode.height / 2;
-
-    const xx1 = (x1 - x2) / (2 * w) - (y1 - y2) / (2 * h);
-    const yy1 = (x1 - x2) / (2 * w) + (y1 - y2) / (2 * h);
-    const a = 1 / (Math.abs(xx1) + Math.abs(yy1));
-    const xx3 = a * xx1;
-    const yy3 = a * yy1;
-    const x = w * (xx3 + yy3) + x2;
-    const y = h * (-xx3 + yy3) + y2;
-
-    return { x, y };
-}
-
-function getEdgePosition(node: Node, intersectionPoint: XYPosition) {
-    const n = { ...node.positionAbsolute, ...node };
-    const nx = Math.round(n.x);
-    const ny = Math.round(n.y);
-    const px = Math.round(intersectionPoint.x);
-    const py = Math.round(intersectionPoint.y);
-
-    if (px <= nx + 1) {
-        return Position.Left;
-    }
-    if (px >= nx + n.width - 1) {
-        return Position.Right;
-    }
-    if (py <= ny + 1) {
-        return Position.Top;
-    }
-    if (py >= n.y + n.height - 1) {
-        return Position.Bottom;
-    }
-
-    return Position.Top;
-}
-
-export function getParallelEdgeParams(source: Node, target: Node, distance: number) {
-    const intersects = {
-        original: {
-            sourceIntersect: getNodeIntersection(source, target),
-            targetIntersect: getNodeIntersection(target, source),
-        },
-        parallel: {
-            sourceIntersect: getParallelNodeIntersection(source, target, distance),
-            targetIntersect: getParallelNodeIntersection(target, source, distance)
-        }
+    const directionVector = { x: p2.x - p1.x, y: p2.y - p1.y };
+    const magnitude = Math.sqrt(directionVector.x ** 2 + directionVector.y ** 2);
+    const unitPerpendicularVector = {
+        x: -directionVector.y / magnitude,
+        y: directionVector.x / magnitude,
     };
 
-    const sourcePos = getEdgePosition(
-        source,
-        {
-            x: intersects.original.sourceIntersect.x,
-            y: intersects.original.sourceIntersect.y,
+    return {
+        parallelP1: {
+            x: p1.x + unitPerpendicularVector.x * offsetDistance,
+            y: p1.y + unitPerpendicularVector.y * offsetDistance,
+        },
+        parallelP2: {
+            x: p2.x + unitPerpendicularVector.x * offsetDistance,
+            y: p2.y + unitPerpendicularVector.y * offsetDistance,
         }
-    );
-    return { intersects: intersects, sourcePos };
+    };
 }
+
+function getNodeCenter(node: Node) {
+    const { width: w, height: h, positionAbsolute: pos } = node;
+
+    return {
+        x: pos.x + w / 2,
+        y: pos.y + h / 2
+    };
+}
+
+function findIntersectionsWithNode(node: Node, x1: number, y1: number, x2: number, y2: number) {
+    const { width: w, height: h, positionAbsolute: pos } = node;
+
+    const left = pos.x, right = pos.x + w, top = pos.y, bottom = pos.y + h;
+    const m = (y2 - y1) / (x2 - x1);
+    const c = y1 - m * x1;
+    let intersections = [];
+
+    if (x2 !== x1) {
+        // Top side
+        let intersectX = (top - c) / m;
+        if (intersectX >= left && intersectX <= right) intersections.push({x: intersectX, y: top});
+
+        // Bottom side
+        intersectX = (bottom - c) / m;
+        if (intersectX >= left && intersectX <= right) intersections.push({x: intersectX, y: bottom});
+    }
+
+    if (y2 !== y1) {
+        // Left side
+        let intersectY = m * left + c;
+        if (intersectY >= top && intersectY <= bottom) intersections.push({x: left, y: intersectY});
+
+        // Right side
+        intersectY = m * right + c;
+        if (intersectY >= top && intersectY <= bottom) intersections.push({x: right, y: intersectY});
+    }
+
+    if (x1 === x2) { // Vertical line
+        intersections.push({x: x1, y: top}, {x: x1, y: bottom});
+    }
+    if (y1 === y2) { // Horizontal line
+        intersections.push({x: left, y: y1}, {x: right, y: y1});
+    }
+
+    return intersections;
+}
+
+function distanceBetween(point1: XYPosition, point2: XYPosition) {
+    return Math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2);
+}
+
+export const calculateNodeIntersections = (sourceNode: Node, targetNode: Node, p1: XYPosition, p2: XYPosition) => {
+    let sourceNodeIntersects = findIntersectionsWithNode(sourceNode, p1.x, p1.y, p2.x, p2.y);
+    let targetNodeIntersects = findIntersectionsWithNode(targetNode, p1.x, p1.y, p2.x, p2.y);
+
+    const center1 = getNodeCenter(sourceNode);
+    const center2 = getNodeCenter(targetNode);
+
+    let closestSourceNodeIntersects = sourceNodeIntersects.reduce((a, b) =>
+        distanceBetween(center2, a) < distanceBetween(center2, b) ? a : b);
+
+    let closestTargetNodeIntersects = targetNodeIntersects.reduce((a, b) =>
+        distanceBetween(center1, a) < distanceBetween(center1, b) ? a : b);
+
+    return { sourceNodeIntersectPos: closestSourceNodeIntersects, targetNodeIntersectPos: closestTargetNodeIntersects };
+}
+
+export const getBetterFloatingStraightPath = (targetNode: Node, sourceNode: Node, parallel: boolean = false, distance: number = 10 ) => {
+    const sourceCenter = getNodeCenter(sourceNode);
+    const targetCenter = getNodeCenter(targetNode);
+
+    const { sourceNodeIntersectPos, targetNodeIntersectPos } = calculateNodeIntersections(sourceNode, targetNode, sourceCenter, targetCenter);
+
+    let path = "";
+    let parallelPath = "";
+    if (parallel) {
+        const parallelToCenterLine = createParallelLine(sourceCenter, targetCenter, distance);
+        const { sourceNodeIntersectPos, targetNodeIntersectPos } = calculateNodeIntersections(sourceNode, targetNode, parallelToCenterLine.parallelP1, parallelToCenterLine.parallelP2);
+        parallelPath += `M ${sourceNodeIntersectPos.x} ${sourceNodeIntersectPos.y} L ${targetNodeIntersectPos.x} ${targetNodeIntersectPos.y}`;
+    }
+
+    path += `M ${sourceNodeIntersectPos.x} ${sourceNodeIntersectPos.y} L ${targetNodeIntersectPos.x} ${targetNodeIntersectPos.y}`
+    return { path, parallelPath };
+};
+
+
+export const getREALStraightPath = (targetNode: Node, sourceNode: Node, parallel: boolean = false, distance: number = 10 ) => {
+    const sourceCenter = getNodeCenter(sourceNode);
+    const targetCenter = getNodeCenter(targetNode);
+
+    let path = "";
+    if (parallel) {
+        const { parallelP1, parallelP2  } = createParallelLine(sourceCenter, targetCenter, distance);
+        path += `M ${parallelP1.x} ${parallelP1.y} L ${parallelP2.x} ${parallelP2.y}`;
+    }
+
+    path += `M ${sourceCenter.x} ${sourceCenter.y} L ${targetCenter.x} ${targetCenter.y}`
+    return path;
+};
 
 
